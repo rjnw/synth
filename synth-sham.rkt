cca658abf4867bf0e90038b861bd81e3d701bd04
Rajan <walrajan@gmail.com>
2018-08-16 22:23:59 -0400

    sham sine wave and synthesize single note

--- synth-sham.rkt
+++ synth-sham.rkt
@@ -2,45 +2,78 @@
 
 (require "../sham/main.rkt")
 
-(define sampling-frequence (make-parameter 44100))
+(define sampling-frequency (make-parameter 44100))
 (define bits-per-sample (make-parameter 16))
 
 (define (freq->sample-period freq)
-  (round (/ (sampling-frequence)
+  (round (/ (sampling-frequency)
             freq)))
-(defien (seconds->samples s)
-  (inexact->exact (round (* s fs))))
+(define (seconds->samples s)
+  (inexact->exact (round (* s (sampling-frequency)))))
 
-(define (sine-sym freq)
-  (string->symbol (format "sine-wave-~a" freq)))
-(define (sine-wave-function freq)
-  (s$:dfunction (void) (sine-sym freq)
-                (list 'x) (list s$:i32) s$:f32
-                (s$:ret (s$:app^ (s$:rs 'sin)
-                                 (s$:app^ (s$:rs 'mul-nfw)
-                                          (s$:f32 (exact->inexact (/ (* freq 2.0 pi)
-                                                                     (sampling-frequency))))
-                                          (s$:rs 'i32->f32 (s$:v 'x)))))
-                ))
+(define wave-type (s$:tptr (s$:tfun (list s$:f32 s$:i32) s$:f32)))
 
-(define (sawtooth-sym freq)
-  (string->symbol (format "sawtooth-wave-~a" freq)))
-(define (sawtooth-wave-function freq)
-  (define sample-period (freq->sample-period freq))
-  (define sample-period/2 (quotient sample-period 2))
-  (define sample-period/4 (quotient sample-period 4))
-  (s$:dfunction (void) (sawtooth-sym freq)
-                (list 'x) (list s$:i32) s$:f32
-                (s$:ret (s$:app^ (s$:rs '-)
-                                 (s$:app^ (s$:rs '/)
-                                          (... (exact->inexact (modulo x sample-period)))
-                                          sample-period/2)
-                                 (s$:f32 1.0)))))
+(define sine-wave-function
+  (s$:dfunction
+   (void) 'sine-wave
+   (list 'x 'freq) (list s$:i32 s$:f32) s$:f32
+   (s$:ret (s$:app^ (s$:ri 'llvm.sin.f32 s$:f32)
+                    (s$:app^ s$:fmul
+                             (s$:app^ s$:fdiv (s$:app^ s$:fmul (s$:v 'freq) (s$:fl32 (* 2.0 pi)))
+                                      (s$:fl32 (exact->inexact (sampling-frequency))))
+                             (s$:app^ s$:ui->fp (s$:v 'x) (s$:etype s$:f32)))))))
 
-(define (sequence-signals)
-  (s$:dfunction (void) 'sequence '(pattern tempo wave)))
+(define (load-signal s i)
+  (s$:app^  s$:load (s$:gep^ s i)))
+(define (add-signal! s i v)
+  (s$:app^ s$:store! (s$:app^ s$:fadd
+                              (load-signal s i) v)
+           (s$:gep^ s i)))
 
 (define synthesize-note
-  (s$:dfunction (void) 'synthesize-note
-                '(note-freq nsamples wave)
-                (list s$:f32 s$:i32 (s$:tfun s:f32 s:i32 s$:f32))))
+  (s$:dfunction
+   (void) 'synthesize-note
+   '(freq nsamples wavef output output-startpos)
+   (list s$:f32 s$:i32 wave-type s$:f32* s$:i32) s$:void
+   (s$:se
+    (s$:slet1^
+     'ni s$:i32 (s$:ui32 0)
+     (s$:while-ule^ (s$:v 'ni) (s$:v 'nsamples)
+                    (s$:se
+                     (add-signal! (s$:v 'output)
+                                  (s$:app^ s$:add-nuw (s$:v 'ni) (s$:v 'output-startpos))
+                                  (s$:app^ (s$:rs 'wavef)  (s$:v 'ni) (s$:v 'freq))))
+                    (s$:set! (s$:v 'ni) (s$:app^ s$:add-nuw (s$:v 'ni) (s$:ui32 1))))
+     s$:ret-void))))
+
+(module+ test
+
+  (require ffi/unsafe
+           rackunit)
+  (define test-module
+    (s$:dmodule
+     (empty-mod-env-info) 'test-module
+     (list
+      sine-wave-function
+      synthesize-note
+      )))
+
+  (define mod-env (compile-module test-module))
+  (jit-verify-module mod-env)
+  (jit-dump-module mod-env)
+  (optimize-module mod-env #:opt-level 3 #:size-level 2)
+  (jit-dump-module mod-env)
+
+  (initialize-jit! mod-env)
+
+  (define s-note (jit-get-function 'synthesize-note mod-env))
+  (define sine-wave (jit-get-function 'sine-wave mod-env))
+  (define sine-wavef-ptr (jit-get-function-ptr 'sine-wave mod-env))
+
+  (define tempo 3800)
+  (define samples-per-beat (quotient (* (sampling-frequency) 60) tempo))
+  (define mblock (malloc _float (* samples-per-beat 2)))
+  (memset mblock 0 (* samples-per-beat 2) _float)
+  (s-note 4.0 10 sine-wavef-ptr mblock 4)
+  (check-= (ptr-ref mblock _float 6) (sine-wave 2 4.0) 0.00000001)
+  )
