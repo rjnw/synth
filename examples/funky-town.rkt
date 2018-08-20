7c63d561faf686b4f5e7a57fa3d8123fe5b30897
rajan <wal.rajan@gmail.com>
2018-08-20 09:57:08 -0400

    formatting examples

--- examples/funky-town.rkt
+++ examples/funky-town.rkt
@@ -4,8 +4,11 @@
 #:bpm 380
 
 (sequence
-  sawtooth-wave #:times 2
-  [(C 5) #f (C 5) #f (A# 4) #f (C 5) (#f 3) (G 4) (#f 3)
-   (G 4) #f (C 5) #f (F 5) #f (E 5) #f (C 5) (#f 9)])
+  sawtooth-wave #:times 1
+  [(C 5)
+   #f
+   (C 5) #f (A# 4) #f (C 5) (#f 3) (G 4) (#f 3)
+   (G 4) #f (C 5) #f (F 5) #f (E 5) #f (C 5) (#f 9)
+   ])
 
-(drum #:times 8 (O #f #f #f X #f #f #f))
+;; (drum #:times 8 (O #f #f #f X #f #f #f))
--- examples/smoke-on-the-water.rkt
+++ examples/smoke-on-the-water.rkt
@@ -12,7 +12,7 @@
            (chord D 3 1 custom 3 8) #f
            (chord D 3 1 custom 6 11)
            (chord D 3 2 custom 5 10) (#f 2)
-           
+
            (chord D 3 1 custom 0 5) #f
            (chord D 3 1 custom 3 8) #f
            (chord D 3 2 custom 5 10) #f
