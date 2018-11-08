c1043319a723a5e791d4c8de69a8ef46e7fc9888
rajan <wal.rajan@gmail.com>
2018-11-08 07:31:42 -0500

    interp funky-town

---
+++ examples/funky-town-interp.rkt
@@ -0,0 +1,15 @@
+#lang synth
+
+#:output "funky-town.wav"
+#:bpm 380
+#:runner 'interp
+
+(sequence
+  sawtooth-wave #:times 1
+  [(C 5)
+   #f
+   (C 5) #f (A# 4) #f (C 5) (#f 3) (G 4) (#f 3)
+   (G 4) #f (C 5) #f (F 5) #f (E 5) #f (C 5) (#f 9)
+   ])
+
+(drum #:times 8 (O #f #f #f X #f #f #f))
